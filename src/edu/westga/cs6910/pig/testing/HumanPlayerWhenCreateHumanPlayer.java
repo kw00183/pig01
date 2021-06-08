968e561c4564c456533ee4f2392f13009b815412
Kim Weible <kw00183@my.westga.edu>
2021-06-08 18:45:54 -0500

    HumanPlayerWhenCreateHumanPlayer - create the JUnit test file and add in Junit test method for the constructor

---
+++ src/edu/westga/cs6910/pig/testing/HumanPlayerWhenCreateHumanPlayer.java
@@ -0,0 +1,29 @@
+package edu.westga.cs6910.pig.testing;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import edu.westga.cs6910.pig.model.HumanPlayer;
+
+/**
+ * This is the JUnit testing class to test the constructor in the HumanPlayer
+ * class.
+ * 
+ * @author Kim Weible
+ * @version Summer 2021
+ *
+ */
+public class HumanPlayerWhenCreateHumanPlayer {
+
+	/**
+	 * SUNNY-DAY Test the constructor accepts the name and initializes the
+	 * HumanPlayer object
+	 */
+	@Test
+	public void testCreateHumanPlayer() {
+		HumanPlayer player = new HumanPlayer("Human");
+
+		assertEquals("Human", player.getName());
+	}
+}
