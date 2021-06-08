d3441246cf0c9a86edcaec01e8d76361233f18bd
Kim Weible <kw00183@my.westga.edu>
2021-06-08 18:47:07 -0500

    HumanPlayerWhenResetTurnTotal - create the JUnit test class and add in test method to test the resetTurnTotal method

---
+++ src/edu/westga/cs6910/pig/testing/HumanPlayerWhenResetTurnTotal.java
@@ -0,0 +1,30 @@
+package edu.westga.cs6910.pig.testing;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import edu.westga.cs6910.pig.model.HumanPlayer;
+
+/**
+ * This is the JUnit testing class to test the resetTurnTotal method in the
+ * HumanPlayer class.
+ * 
+ * @author Kim Weible
+ * @version Summer 2021
+ *
+ */
+public class HumanPlayerWhenResetTurnTotal {
+
+	/**
+	 * SUNNY-DAY Test reseting the turn total for the human player
+	 */
+	@Test
+	public void testResetTurnTotal() {
+		HumanPlayer player = new HumanPlayer("Human");
+
+		player.resetTurnTotal();
+
+		assertEquals(0, player.getTurnTotal());
+	}
+}
