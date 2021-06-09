f5b3457a05b7f4336e23767528b14a6a4d8ad0f3
Kim Weible <kw00183@my.westga.edu>
2021-06-08 20:46:31 -0500

    GameWhenCreateGame - create new JUnit test class to test contructor of the Game class

---
+++ src/edu/westga/cs6910/pig/testing/GameWhenCreateGame.java
@@ -0,0 +1,33 @@
+package edu.westga.cs6910.pig.testing;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import edu.westga.cs6910.pig.model.ComputerPlayer;
+import edu.westga.cs6910.pig.model.Game;
+import edu.westga.cs6910.pig.model.HumanPlayer;
+
+/**
+ * This is the JUnit testing class to test the constructor in the Game class.
+ * 
+ * @author Kim Weible
+ * @version Summer 2021
+ *
+ */
+public class GameWhenCreateGame {
+
+	/**
+	 * SUNNY-DAY Test the constructor accepts the players, initializes them and
+	 * returns the player names with getter
+	 */
+	@Test
+	public void testCreateGame() {
+		HumanPlayer theHuman = new HumanPlayer("Human");
+		ComputerPlayer theComputer = new ComputerPlayer();
+		Game theGame = new Game(theHuman, theComputer);
+
+		assertEquals("Human", theGame.getHumanPlayer().getName());
+		assertEquals("Simple computer", theGame.getComputerPlayer().getName());
+	}
+}
