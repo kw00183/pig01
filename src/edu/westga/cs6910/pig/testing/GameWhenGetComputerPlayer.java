9c12ee5c60cd53f69796ed0fc845dc61497945a0
Kim Weible <kw00183@my.westga.edu>
2021-06-08 21:25:09 -0500

    GameWhenGetComputerPlayer - create JUnit test class for the getComputerPlayer method in the Game class

---
+++ src/edu/westga/cs6910/pig/testing/GameWhenGetComputerPlayer.java
@@ -0,0 +1,34 @@
+package edu.westga.cs6910.pig.testing;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import edu.westga.cs6910.pig.model.ComputerPlayer;
+import edu.westga.cs6910.pig.model.Game;
+import edu.westga.cs6910.pig.model.HumanPlayer;
+
+/**
+ * This is the JUnit testing class to test the getComputerPlayer method in the
+ * Game class.
+ * 
+ * @author Kim Weible
+ * @version Summer 2021
+ *
+ */
+public class GameWhenGetComputerPlayer {
+
+	/**
+	 * SUNNY-DAY Test the getComputerPlayer method with theComputer
+	 */
+	@Test
+	public void testGetComputerPlayer() {
+		HumanPlayer theHuman = new HumanPlayer("Human");
+		ComputerPlayer theComputer = new ComputerPlayer();
+		Game theGame = new Game(theHuman, theComputer);
+
+		theGame.startNewGame(theComputer);
+		
+		assertEquals("Simple computer", theGame.getComputerPlayer().getName());
+	}
+}
