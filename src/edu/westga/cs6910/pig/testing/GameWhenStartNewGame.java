46a7dd4bb22a7778e3c2cb09eec234fc10e20caa
Kim Weible <kw00183@my.westga.edu>
2021-06-08 20:53:46 -0500

    GameWhenStartNewGame - create JUnit test class to test the startNewGame method in the Game class using a human player and then a computer player to start

---
+++ src/edu/westga/cs6910/pig/testing/GameWhenStartNewGame.java
@@ -0,0 +1,48 @@
+package edu.westga.cs6910.pig.testing;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import edu.westga.cs6910.pig.model.ComputerPlayer;
+import edu.westga.cs6910.pig.model.Game;
+import edu.westga.cs6910.pig.model.HumanPlayer;
+
+/**
+ * This is the JUnit testing class to test the startNewGame method in the Game
+ * class.
+ * 
+ * @author Kim Weible
+ * @version Summer 2021
+ *
+ */
+public class GameWhenStartNewGame {
+
+	/**
+	 * SUNNY-DAY Test the startNewGame method with theHuman player
+	 */
+	@Test
+	public void testStartNewGameHuman() {
+		HumanPlayer theHuman = new HumanPlayer("Human");
+		ComputerPlayer theComputer = new ComputerPlayer();
+		Game theGame = new Game(theHuman, theComputer);
+
+		theGame.startNewGame(theHuman);
+
+		assertEquals("Human", theGame.getCurrentPlayer().getName());
+	}
+
+	/**
+	 * SUNNY-DAY Test the startNewGame method with theComputer player
+	 */
+	@Test
+	public void testStartNewGameComputer() {
+		HumanPlayer theHuman = new HumanPlayer("Human");
+		ComputerPlayer theComputer = new ComputerPlayer();
+		Game theGame = new Game(theHuman, theComputer);
+
+		theGame.startNewGame(theComputer);
+
+		assertEquals("Simple computer", theGame.getCurrentPlayer().getName());
+	}
+}
