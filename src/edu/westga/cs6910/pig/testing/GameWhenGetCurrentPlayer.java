2a829bb23231af269549febee8331c6e8c8e1307
Kim Weible <kw00183@my.westga.edu>
2021-06-08 21:28:42 -0500

    GameWhenGetCurrentPlayer - create JUnit test class for testing the getCurrentPlayer method in the Game class

---
+++ src/edu/westga/cs6910/pig/testing/GameWhenGetCurrentPlayer.java
@@ -0,0 +1,48 @@
+package edu.westga.cs6910.pig.testing;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import edu.westga.cs6910.pig.model.ComputerPlayer;
+import edu.westga.cs6910.pig.model.Game;
+import edu.westga.cs6910.pig.model.HumanPlayer;
+
+/**
+ * This is the JUnit testing class to test the getCurrentPlayer method in the
+ * Game class.
+ * 
+ * @author Kim Weible
+ * @version Summer 2021
+ *
+ */
+public class GameWhenGetCurrentPlayer {
+
+	/**
+	 * SUNNY-DAY Test the getCurrentPlayer method with theComputer
+	 */
+	@Test
+	public void testGetCurrentPlayerComputer() {
+		HumanPlayer theHuman = new HumanPlayer("Human");
+		ComputerPlayer theComputer = new ComputerPlayer();
+		Game theGame = new Game(theHuman, theComputer);
+
+		theGame.startNewGame(theComputer);
+
+		assertEquals("Simple computer", theGame.getCurrentPlayer().getName());
+	}
+
+	/**
+	 * SUNNY-DAY Test the getCurrentPlayer method with theHuman
+	 */
+	@Test
+	public void testGetCurrentPlayerHuman() {
+		HumanPlayer theHuman = new HumanPlayer("Human");
+		ComputerPlayer theComputer = new ComputerPlayer();
+		Game theGame = new Game(theHuman, theComputer);
+
+		theGame.startNewGame(theHuman);
+
+		assertEquals("Human", theGame.getCurrentPlayer().getName());
+	}
+}
