4fc5ed57eda519324aad0d720d1ee3b0b2311ff1
Kim Weible <kw00183@my.westga.edu>
2021-06-08 19:13:19 -0500

    HumanPlayerWhenSetIsMyTurn - create the JUnit test class for the setIsMyTurn extended to the HumanPlayer class

---
+++ src/edu/westga/cs6910/pig/testing/HumanPlayerWhenSetIsMyTurn.java
@@ -0,0 +1,44 @@
+package edu.westga.cs6910.pig.testing;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import edu.westga.cs6910.pig.model.HumanPlayer;
+
+/**
+ * This is the JUnit testing class to test the setIsMyTurn method extended to
+ * the HumanPlayer class.
+ * 
+ * @author Kim Weible
+ * @version Summer 2021
+ *
+ */
+public class HumanPlayerWhenSetIsMyTurn {
+
+	/**
+	 * SUNNY-DAY Test passing true to the setIsMyTurn method for the human
+	 * player
+	 */
+	@Test
+	public void testSetIsMyTurnPassTrue() {
+		HumanPlayer player = new HumanPlayer("Human");
+
+		player.setIsMyTurn(true);
+
+		assertEquals(true, player.getIsMyTurn());
+	}
+
+	/**
+	 * SUNNY-DAY Test passing false to the setIsMyTurn method for the human
+	 * player
+	 */
+	@Test
+	public void testSetIsMyTurnPassFalse() {
+		HumanPlayer player = new HumanPlayer("Human");
+
+		player.setIsMyTurn(false);
+
+		assertEquals(false, player.getIsMyTurn());
+	}
+}
