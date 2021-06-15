2a9f82598d0f304a875225f7b75b8168a1875d27
Kim Weible <kw00183@my.westga.edu>
2021-06-15 08:31:56 -0500

    TestWhenDicePairIsRolled - add in additional test methods to check the dice pair rolls more than a 1 at least 1 time in 10000 times

--- src/edu/westga/cs6910/pig/testing/dicepair/TestWhenDicePairIsRolled.java
+++ src/edu/westga/cs6910/pig/testing/dicepair/TestWhenDicePairIsRolled.java
@@ -1,5 +1,6 @@
 package edu.westga.cs6910.pig.testing.dicepair;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.Test;
@@ -46,5 +47,41 @@ public class TestWhenDicePairIsRolled {
 				fail("Rolled a die with value: " + testDicePair.getDie2Value());
 			}
 		}
+	}
+	
+	/**
+	 * SUNNY-DAY Test the rollDice method after the dice pair is created and roll die 1
+	 * 10000 times and get a 2 at least 1 time (just need it to roll something
+	 * other than the start value of 1)
+	 */
+	@Test
+	public void testDice1Rolled10000Rolls2() {
+		boolean rolled2 = false;
+		DicePair testDicePair = new DicePair();
+		for (int count = 0; count < 10000; count++) {
+			testDicePair.rollDice();
+			if (testDicePair.getDie1Value() == 2) {
+				rolled2 = true;
+			}
+		}
+		assertEquals(true, rolled2);
+	}
+	
+	/**
+	 * SUNNY-DAY Test the rollDice method after the dice pair is created and roll die 2
+	 * 10000 times and get a 2 at least 1 time (just need it to roll something
+	 * other than the start value of 1)
+	 */
+	@Test
+	public void testDice2Rolled10000Rolls2() {
+		boolean rolled2 = false;
+		DicePair testDicePair = new DicePair();
+		for (int count = 0; count < 10000; count++) {
+			testDicePair.rollDice();
+			if (testDicePair.getDie2Value() == 2) {
+				rolled2 = true;
+			}
+		}
+		assertEquals(true, rolled2);
 	}
 }
