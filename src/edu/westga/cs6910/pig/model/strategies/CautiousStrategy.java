3ae6c2271a248902b902df4aa2f0d34049619d3b
Kim Weible <kw00183@my.westga.edu>
2021-06-21 12:10:27 -0500

    CautiousStrategy - create CautiousStrategy class and add in override method for rollAgain that always returns false

---
+++ src/edu/westga/cs6910/pig/model/strategies/CautiousStrategy.java
@@ -0,0 +1,17 @@
+package edu.westga.cs6910.pig.model.strategies;
+
+/**
+ * Class for the cautious pig strategy that implements the PigStrategy interface
+ * 
+ * @author Kim Weible
+ * @version Summer 2021
+ *
+ */
+public class CautiousStrategy implements PigStrategy {
+
+	@Override
+	public boolean rollAgain(int numberOfTurnRolls, int numberOfTurnPoints,
+			int differenceBetweenTurnAndGoalPoints) {
+		return false;
+	}
+}
